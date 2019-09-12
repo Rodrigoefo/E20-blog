c458e4c4b9440a1633cf43c678ba945495550800
Rodrigo <fuentesosorio@fuentesosorio.com>
2019-09-12 15:30:06 -0300

    Added authorize to pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
-  
+
 
   def index
     @users = User.all
+    authorize! :manage, @pages
+
   end
 
 def update
+authorize! :update, @pages
  @user = User.find(params[:id])
   if @user.visit?
     @user.admin!
